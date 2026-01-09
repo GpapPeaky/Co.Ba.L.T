b26c3fbcdc7ec455face20b6bdd0861760554b26
GpapPeaky <giorgospapamat@gmail.com>
2026-01-09 04:46:35 +0200

    Added window icon, removed the built-in exit key

--- CBLT_Win.cpp
+++ CBLT_Win.cpp
@@ -6,12 +6,21 @@ namespace CBLT {
     namespace Win {
 
         void Init(void) {
+            Image icon = LoadImage("assets/icon/muse.png");
+            
             const int screenWidth = 1536;
             const int screenHeight = 1024;
-
+            
             InitWindow(screenWidth, screenHeight, Version.c_str());
-
+            
+            SetWindowIcon(icon);
+            
+            // Free image
+            UnloadImage(icon);
+            
             SetTargetFPS(180);
+
+            SetExitKey(KEY_NULL); // Disable automatic ESC exit
         }
 
         void Destroy(void) {
